3bcf9349a13f850510f5e1cd9dee397e0db205dd
Ruan Bezerra da Silva <ruan.silva@lead.fortaleza>
2022-02-02 12:39:23 -0300

    Implemente User in Models

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank.Web.API.Models
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public int account { get; set; }
+        public int passoword { get; set; }
+        public int AgenciAccount { get; set; }
+    }
+}
